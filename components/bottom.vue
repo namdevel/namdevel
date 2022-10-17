f69b1a451809471b6926b4f208f30508ce60a4e1
几句レし <namdevel@gmail.com>
2022-10-17 20:15:41 +0700

    chore : social media footer links

--- components/bottom.vue
+++ components/bottom.vue
@@ -6,5 +6,8 @@
         <li class="nav-item"><a href="#" class="nav-link px-2 text-muted dark:text-white">FAQs</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-2 text-muted dark:text-white">About</a></li>
     </ul>
-    <p class="text-center text-muted">© 2022 Company, Inc</p>
+    <p class="text-center text-muted">Made with a cup of <i
+            class="fa-duotone fa-mug-hot ms-2 me-2 text-green fa-fade"></i>
+        by
+        NAMDEVEL</p>
 </template>
