c0f301f34f1d1af6dbd63b270efb70f8e25d950c
几句レし <namdevel@gmail.com>
2022-10-18 19:49:16 +0700

    fix: toggle giscus themes

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,3 +1,29 @@
+<script setup>
+const colorMode = useColorMode()
+const themes = colorMode.preference === 'light' ? 'light' : 'dark';
+useHead({
+    script: [
+        {
+
+            src: "https://giscus.app/client.js",
+            'data-repo': "namdevel/namdevel",
+            'data-repo-id': "R_kgDOIPUbFA",
+            'data-category': "General",
+            'data-category-id': "DIC_kwDOIPUbFM4CSB-A",
+            'data-mapping': "og:image",
+            'data-strict': "0",
+            'data-reactions-enabled': "0",
+            'data-emit-metadata': "0",
+            'data-input-position': "top",
+            'data-theme': themes,
+            'data-lang': "en",
+            'data-loading': "lazy",
+            'crossorigin': "anonymous",
+            async: true, body: true
+        }
+    ],
+});
+</script>
 <template>
     <div>
         <header>
